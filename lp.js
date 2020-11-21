5606c5d4a5b60f4fe768c18f77d6d67d4e1af1f5
program-mae <74656391+program-mae@users.noreply.github.com>
2020-11-21 10:51:20 +0900

    Create lp.js

---
+++ lp.js
@@ -0,0 +1,21 @@
+var scrollAnimationElm = document.querySelectorAll('.sa');
+var scrollAnimationFunc = function() {
+  for(var i = 0; i < scrollAnimationElm.length; i++) {
+    var triggerMargin = 600;
+    var elm = scrollAnimationElm[i];
+    var showPos = 0;
+    if(elm.dataset.sa_margin != null) {
+      triggerMargin = parseInt(elm.dataset.sa_margin);
+    }
+    if(elm.dataset.sa_trigger) {
+      showPos = document.querySelector(elm.dataset.sa_trigger).getBoundingClientRect().top + triggerMargin;
+    } else {
+      showPos = elm.getBoundingClientRect().top + triggerMargin;
+    }
+    if (window.innerHeight > showPos) {
+      elm.classList.add('show');
+    }
+  }
+}
+window.addEventListener('load', scrollAnimationFunc);
+window.addEventListener('scroll', scrollAnimationFunc);
