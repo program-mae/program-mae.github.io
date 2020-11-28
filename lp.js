6f927e0005ce5d94e7fa20ce80517af5b6d1d23c
program-mae <74656391+program-mae@users.noreply.github.com>
2020-11-28 21:38:21 +0900

    Update lp.js

--- lp.js
+++ lp.js
@@ -1,19 +1,29 @@
-var scrollAnimationElm = document.querySelectorAll('.sa');
+var scrollAnimationClass = 'sa';
+var scrollAnimationShowClass = 'show';
+var triggerMarginDefault = 0;
+ 
+var scrollAnimationElm = document.querySelectorAll('.' + scrollAnimationClass);
 var scrollAnimationFunc = function() {
+  var dataMargin = scrollAnimationClass + '_margin';
+  var dataTrigger = scrollAnimationClass + '_trigger';
+  var dataDelay = scrollAnimationClass + '_delay';
   for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 600;
+    var triggerMargin = triggerMarginDefault;
     var elm = scrollAnimationElm[i];
     var showPos = 0;
-    if(elm.dataset.sa_margin != null) {
-      triggerMargin = parseInt(elm.dataset.sa_margin);
+    if(elm.dataset[dataMargin] != null) {
+      triggerMargin = parseInt(elm.dataset[dataMargin]);
     }
-    if(elm.dataset.sa_trigger) {
-      showPos = document.querySelector(elm.dataset.sa_trigger).getBoundingClientRect().top + triggerMargin;
+    if(elm.dataset[dataTrigger]) {
+      showPos = document.querySelector(elm.dataset[dataTrigger]).getBoundingClientRect().top + triggerMargin;
     } else {
       showPos = elm.getBoundingClientRect().top + triggerMargin;
     }
     if (window.innerHeight > showPos) {
-      elm.classList.add('show');
+      var delay = (elm.dataset[dataDelay])? elm.dataset[dataDelay] : 0;
+      setTimeout(function(index) {
+        scrollAnimationElm[index].classList.add('show');
+      }.bind(null, i), delay);
     }
   }
 }
